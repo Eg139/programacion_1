fa276312f8668271997e93b214a9dd51b2a54402
Eg139 <eric.guzman.139@gmail.com>
2023-04-10 21:25:03 -0300

    ej 3 y 4 terminados

--- labo/ejercicios_lista/ejercicio03.py
+++ labo/ejercicios_lista/ejercicio03.py
@@ -1,3 +1,19 @@
 # 3-
 # Realizar una carga indefinida de números, añadirlos a una lista e indicar que posición
 # de memoria es la que mas ocurrencias tiene dentro de esa lista.
+
+numeros = []
+seguir = 's'
+while seguir == 's':
+    try:
+        while True:
+            numero = int(input("Ingrese un numero: "))
+            break
+    except ValueError:
+        print("Trataste de convertir algo diferente a un numero")
+    numeros.append(numero)
+    seguir = input("Desea seguir? s/n: ")
+
+print(f"posicion de memoria de la lista {id(numeros)}")
+for numero in numeros:
+    print(f"posicion de memoria los numeros en la lista {id(numero)}")
--- labo/ejercicios_lista/ejercicio04.py
+++ labo/ejercicios_lista/ejercicio04.py
@@ -6,11 +6,99 @@
 # (validar). El valor por defecto para los senadores ausentes será Abstención.
 # Se deberá Informar:
 
-# o Cantidad total de senadores.
-# o Cantidad de senadores presentes.
-# o Cantidad y porcentaje de votos afirmativos.
-# o Cantidad y porcentaje de votos negativos.
-# o Cantidad y porcentaje de abstenciones.
-# o Generar una lista de senadores por cada tipo de voto y mostrarlas por
-# pantalla.
+nombres = ['Roberto','Marco','Lucia','Carla','Fernando']
+votos = ['Afirmativo','Negativo','Negativo','Abstencion','Abstencion']
+asistencias = ['Presente','Presente','Presente','Ausente','Presente']
+
+def alta_senador():
+        nombre = input("Ingrese el nombre: ")
+        asistencia = input("Ingrese la asistencia del senador(Presente o Ausente): ")
+        if asistencia == 'Ausente':
+            voto = 'Abstencion'
+        else:
+            while True:
+                voto = input("Afirmativo, Negativo o Abstencion: ")
+                if voto != 'Afirmativo' and voto != 'Negativo' and voto != 'Abstencion':
+                    break
+                else:
+                    print("Opcion invalida.")
+        nombres.append(nombre)
+        asistencias.append(asistencia)
+        votos.append(voto)
+
+while True:
+    
+    tam = len(votos)
+    print("# A. Cantidad total de senadores.")
+    print("# B. Cantidad de senadores presentes.")
+    print("# C. Cantidad y porcentaje de votos afirmativos.")
+    print("# D. Cantidad y porcentaje de votos negativos.")
+    print("# E. Cantidad y porcentaje de abstenciones.")
+    print("# F. Generar una lista de senadores por cada tipo de voto y mostrarlas por pantalla.")
+    print("# G Ingresar senadores")
+    print("# S. salir")
+    opcion = input("Ingrese una opcion:  ")
+    if opcion == 'A':
+        contador = 0
+        for i in range(0, tam):
+            contador += 1
+        print(f"La cantidad de senadores es de {contador}")
+    elif opcion == 'B':
+        contador = 0
+        for asistencia in asistencias:
+            if asistencia == 'Presente':
+                contador += 1
+        print(f"La cantidad de senadores presentes es de {contador}")
+    elif opcion == 'C':
+        contador = 0
+        for voto in votos:
+            if voto == 'Afirmativo':
+                contador += 1
+        porcentaje = (contador / tam)*100
+        print(f"La cantidad de votos afirmativos es de {contador} con un porcentaje de {porcentaje} %")
+    elif opcion == 'D':
+        contador = 0
+        for voto in votos:
+            if voto == 'Negativo':
+                contador += 1
+        porcentaje = (contador / tam)*100
+        print(f"La cantidad de votos negativos es de {contador} con un porcentaje de {porcentaje} %")
+    elif opcion == 'E':
+        contador = 0
+        for voto in votos:
+            if voto == 'Abstencion':
+                contador += 1
+        porcentaje = (contador / tam)*100
+        print(f"La cantidad de votos abstenciones es de {contador} con un porcentaje de {porcentaje} %")        
+    elif opcion == 'F':
+        positivos = []
+        negativos = []
+        abstenciones = []
+        for i in range(0, tam):
+            aux = []
+            if votos[i] == 'Afirmativo':
+                aux.append(nombres[i])
+                aux.append(asistencias[i])
+                aux.append(votos[i])
+                positivos.append(aux)
+            elif votos[i] == 'Negativo':
+                aux.append(nombres[i])
+                aux.append(asistencias[i])
+                aux.append(votos[i])
+                negativos.append(aux)
+            else:
+                aux.append(nombres[i])
+                aux.append(asistencias[i])
+                aux.append(votos[i])
+                abstenciones.append(aux)
+
+        print(f"Lista de afirmativos {positivos}")
+        print(f"Lista de negativos {negativos}")
+        print(f"Lista de abstenciones {abstenciones}")    
+    elif opcion == 'G':
+        alta_senador()
+    elif opcion == 'S':
+        print("Saliendo del programa")
+        
+        break
 
