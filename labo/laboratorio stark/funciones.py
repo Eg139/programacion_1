8848a60ddbf3252ec5a43226d504d54fe676c74c
Eg139 <eric.guzman.139@gmail.com>
2023-06-06 21:19:47 -0300

    desafio stark01 listo

--- labo/laboratorio stark/funciones.py
+++ labo/laboratorio stark/funciones.py
@@ -1,5 +1,14 @@
 import os
-def sacar_max(listado:list,param:str):
+def sacar_max(listado:list,param:str)-> float:
+    """obtiene el valor maximo dentro de una lista de diccionarios
+
+    Args:
+        listado (list): lista para recorrer
+        param (str): nombre de la key a evaluar
+
+    Returns:
+        float: retorna el valor maximo encontrado
+    """
     maximo = 0
     flag = False
     for individuo in listado:
@@ -8,7 +17,16 @@ def sacar_max(listado:list,param:str):
             flag = True
     return maximo
 
-def sacar_min(listado:list,param:str):
+def sacar_min(listado:list,param:str)-> float:
+    """obtiene el valor minimo dentro de una lista de diccionarios
+
+    Args:
+        listado (list): lista para recorrer
+        param (str): nombre de la key a evaluar
+
+    Returns:
+        float: retorna el valor minimo encontrado
+    """
     minimo = 0
     flag = False
     for individuo in listado:
@@ -17,33 +35,79 @@ def sacar_min(listado:list,param:str):
             flag = True
     return minimo
 
-def buscar_float(listado:list,param:str,item_comparar:float):
+def buscar_float(listado:list,param:str,item_comparar:float)->list:
+    """Busca los valores de una key especifica comparando con el parametro dado, de cumplirse las condiciones lo agrega en una nueva lista
+
+    Args:
+        listado (list): lista para recorrer
+        param (str): nombre de la key a evaluar
+        item_comparar (float): elemento a comparar con el valor de la key
+
+    Returns:
+        list: lista con los elemento que cumplen las condiciones
+    """
     aux_list = []
     for individuo in listado:
         if float(individuo[param]) == item_comparar:
             aux_list.append(individuo)
     return aux_list
 
-def buscar_string(listado:list,param:str,item_comparar:str,message:str):
+def buscar_string(listado:list,param:str,item_comparar:str)->list:
+    """Busca los valores de una key especifica comparando con el parametro dado, de cumplirse las condiciones lo agrega en una nueva lista
+
+    Args:
+        listado (list): lista para recorrer
+        param (str): nombre de la key a evaluar
+        item_comparar (str): elemento a comparar con el valor de la key
+
+    Returns:
+        list: lista con los elemento que cumplen las condiciones
+    """
+    aux_list = []
     for individuo in listado:
         if individuo[param] == item_comparar:
-            print(message+ individuo[param])
+            aux_list.append(individuo)
+    return aux_list
 
-def sacar_promedio(listado:list,param:str):
+def sacar_promedio(listado:list,param:str)->float:
+    """_summary_
+
+    Args:
+        listado (list): lista a evaluar
+        param (str): nombre de la key que se va a usar
+
+    Returns:
+        float: promedio de los elementos seleccionados
+    """
     acumulador = 0
     for heroe in listado:
         acumulador += float(heroe[param])
     return acumulador/len(listado)
 
 def mostrar_varios(listado:list,item_buscado:str,message:str):
+    """imprime en consola varios individuos con mensajes
+
+    Args:
+        listado (list): lista a recorrer
+        item_buscado (str): nombre de la key a evaluar
+        message (str): mensaje predefinido que se mostrara
+    """
     for individuo in listado:
         mostrar(individuo[item_buscado],message)
 
 def mostrar(param:any,message:str):
+    """imprime por consola un individuo con un mensaje
+
+    Args:
+        param (any): parametro que se muestra con el mensaje
+        message (str): mensaje que se mostrara
+    """
     print(message.format(param) )
 
 
 def mostrar_menu():
+    """Imprime el menu de opciones del programa
+    """
     print("          ********************    MENU DE OPCIONES    **************************         ")
     print("# A. Analizar detenidamente el set de datos.")
     print("# B. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe.")
@@ -53,11 +117,155 @@ def mostrar_menu():
     print("# F. Recorrer la lista y determinar la altura promedio de los superhéroes (PROMEDIO)")
     print("# G. Informar cual es el Nombre del superhéroe asociado a cada uno de los indicadores anteriores (MÁXIMO, MÍNIMO)")
     print("# H. Calcular e informar cual es el superhéroe más y menos pesado.")
-    print("# I. Ordenar el código implementando una función para cada uno de los valores informados.")
-    print("# J. Construir un menú que permita elegir qué dato obtener.")
+    print("# I. menú que permita elegir qué dato obtener.")
+    print("# J. menu stark 01")
     print("# Z. salir")
 
+def filtrar_elemento(listado:list,key:str,value:any)->dict:
+    aux_dict = {}
+    for individuo in listado:
+        if float(individuo[key]) == value:
+            aux_dict.append(individuo)
+    return aux_dict
+
+def mostrar_menu_stark_01():
+    """Imprime el menu de opciones del programa
+    """
+    print("          ********************    MENU DE OPCIONES    **************************         ")
+    print("# A. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género M.")
+    print("# B. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe de género F")
+    print("# C. Recorrer la lista y determinar cuál es el superhéroe más alto de género M")
+    print("# D. Recorrer la lista y determinar cuál es el superhéroe más alto de género F")
+    print("# E. Recorrer la lista y determinar cuál es el superhéroe más bajo de género M")
+    print("# F. Recorrer la lista y determinar cuál es el superhéroe más bajo de género F")
+    print("# G. Recorrer la lista y determinar la altura promedio de los superhéroes de género M")
+    print("# H. Recorrer la lista y determinar la altura promedio de los superhéroes de género F")
+    print("# I. Informar cual es el Nombre del superhéroe asociado a cada uno de los indicadores anteriores (ítems C a F)")
+    print("# J. Determinar cuántos superhéroes tienen cada tipo de color de ojos.")
+    print("# K. Determinar cuántos superhéroes tienen cada tipo de color de pelo.")
+    print("# L. Determinar cuántos superhéroes tienen cada tipo de inteligencia (En caso de no tener, Inicializarlo con No Tiene).")
+    print("# M. Listar todos los superhéroes agrupados por color de ojos.")
+    print("# N. Listar todos los superhéroes agrupados por color de pelo.")
+    print("# O. Listar todos los superhéroes agrupados por tipo de inteligencia")
+    print("# Z. salir")
+def elegir_menu_stark_01(lista:list):
+    aux_informes = []
+    while True:
+        os.system('cls')
+        mostrar_menu_stark_01()
+        opcion = input("Ingrese una opcion:  ")
+        
+        match opcion:       
+            case "A":
+                aux = buscar_string(lista,"genero","M")
+                mostrar_lista_parcial(aux,"Listado de heroes Masculinos","nombre","genero")
+            case "B":
+                aux = buscar_string(lista,"genero","F")
+                mostrar_lista_parcial(aux,"Listado de heroes Femeninos","nombre","genero") 
+            case "C":
+                aux = buscar_string(lista,"genero","M")
+                max_altura = sacar_max(aux,"altura")
+                aux = buscar_float(aux,"altura",max_altura)
+                aux_informes.append(aux[0]['nombre'])
+
+                
+            case "D":
+                aux = buscar_string(lista,"genero","F")
+                max_altura = sacar_max(aux,"altura")
+                aux = buscar_float(aux,"altura",max_altura)
+                aux_informes.append(aux[0]['nombre'])
+
+            case "E":
+                aux = buscar_string(lista,"genero","M")
+                max_altura = sacar_min(aux,"altura")
+                aux = buscar_float(aux,"altura",max_altura)
+                aux_informes.append(aux[0]['nombre'])
+
+            case "F":
+                aux = buscar_string(lista,"genero","F")
+                max_altura = sacar_min(aux,"altura")
+                aux = buscar_float(aux,"altura",max_altura)
+
+                aux_informes.append(aux[0]['nombre'])
+
+            case "G":
+                aux = buscar_string(lista,"genero","M")
+                promedio = sacar_promedio(aux,"altura")
+                print(f"El promedio de altura de los superheroes M es: {promedio}")
+            case "H":
+                aux = buscar_string(lista,"genero","F")
+                promedio = sacar_promedio(aux,"altura")
+                print(f"El promedio de altura de los superheroes F es: {promedio}")
+            case "I":
+                for heroe in aux_informes:
+                    print(heroe)
+            case "J":
+                aux = crear_lista_srepetidos(lista,"color_ojos")
+                contar_x_categoria(aux,lista,"color_ojos")
+            case "K":
+                aux = crear_lista_srepetidos(lista,"color_pelo")
+                contar_x_categoria(aux,lista,"color_pelo")
+            case "L":
+                aux = inicializar_vacio(lista,"inteligencia","No Tiene")
+                aux = crear_lista_srepetidos(lista,"inteligencia")
+                contar_x_categoria(aux,lista,"inteligencia")                
+            case "M":
+                aux = crear_lista_srepetidos(lista,"color_ojos")
+                mostrar_x_categoria(aux,lista,"color_ojos")
+            case "N":
+                aux = crear_lista_srepetidos(lista,"color_pelo")
+                mostrar_x_categoria(aux,lista,"color_pelo")
+            case "O":
+                aux = crear_lista_srepetidos(lista,"inteligencia")
+                mostrar_x_categoria(aux,lista,"inteligencia")                
+            case "Z":
+                print("Saliendo del programa")    
+                break
+            case _:
+                print("Opcion invalida.")
+        os.system("pause")
+
+def crear_lista_srepetidos(listado:list,item:str)->list:
+    aux = []
+    for heroe in listado:
+        if not filtrar_repetidos(aux,heroe[item]):
+            aux.append(heroe[item])
+    return aux
+
+def filtrar_repetidos(lista:list, item:str)->bool:
+    esta = False
+    for elemento in lista:
+        if (elemento == item):
+            esta = True
+            break
+    return esta
+
+def mostrar_x_categoria(categorias:list,lista:list,item:str):
+    for categoria in categorias:
+        print(f"Lista de {categoria}")
+        for heroe in lista:
+            if heroe[item] == categoria:
+                print(f"{heroe['nombre']}  {heroe['color_pelo']}")
+        print("\n------------------------------------\n")
+
+def contar_x_categoria(categorias:list,lista:list,item:str):
+    for categoria in categorias:
+        print(f"Lista de {categoria}")
+        contador = 0
+        for heroe in lista:
+            if heroe[item] == categoria:
+                contador+=1
+        print(f"Los heroes contados son: {contador}")    
+        print("\n------------------------------------\n")
+
+def inicializar_vacio(lista:list,key:str,message:str):
+    for heroe in lista:
+        if heroe[key] == "":
+            heroe[key] = message
+
 def submenu():
+    """Imprime por consola un sub menu
+    """
     print("          ********************    MENU DE OPCIONES    **************************         ")
     print("# A. nombre.")
     print("# B. identidad.")
@@ -72,6 +280,12 @@ def submenu():
     print("# Z. salir")
 
 def elegir_submenu(listado:list, titulo:str):
+    """Elije una opcion del menu y ejecuta lo solicitado de la opcion
+
+    Args:
+        listado (list): lista a recorrer
+        titulo (str): titulo de las listas que se generan en las opciones
+    """
     while True:
         os.system('cls')
         
@@ -134,6 +348,14 @@ def elegir_submenu(listado:list, titulo:str):
 
 
 def mostrar_lista_parcial(listado:list,titulo:str,param1:str,param2:str=""):
+    """se muestra por consola solo los parametros dados por el usuario de una lista
+
+    Args:
+        listado (list): lista a recorrer
+        titulo (str): encabezado de la lista
+        param1 (str): nombre de la key a mostrar
+        param2 (str, optional): nombre de la key a mostrar. Defaults to "".
+    """
     print(titulo)
     print(f"{param1}\t {param2}\t")
     for individuo in listado:
--- labo/laboratorio stark/main.py
+++ labo/laboratorio stark/main.py
@@ -3,68 +3,71 @@ from data_stark import lista_personajes
 from funciones import *
 
 while True:
+    lista_heroes = []
+    for heroe in lista_personajes:
+        lista_heroes.append(heroe.copy())
+    print(lista_heroes)
     os.system('cls')
     mostrar_menu()
-    tam = len(lista_personajes)
+    tam = len(lista_heroes)
     opcion = input("Ingrese una opcion:  ")
     
-            
-    if opcion == "A":
-        print("Listado de superheroes")
-        print("Nombre\t Identidad\t Empresa\t Altura\t Peso\t")
-        for superheroe in lista_personajes:
-            print(f"{superheroe['nombre']}\t {superheroe['identidad']}\t {superheroe['empresa']}\t {superheroe['altura']}\t {superheroe['peso']}\t")
-    elif opcion == "B":
-        param_mostrar_1 = "nombre"
-        mostrar_lista_parcial(lista_personajes,"Lista de superheroes",param_mostrar_1)
-    elif opcion == "C":
-        param_mostrar_1 = "nombre"
-        param_mostrar_2 = "altura"
-        mostrar_lista_parcial(lista_personajes,"Lista de superheroes",param_mostrar_1,param_mostrar_2)
-    elif opcion == "D":
-        param_buscar = "altura"
-        maximo = sacar_max(lista_personajes,param_buscar)
-        aux_maximo = buscar_float(lista_personajes,param_buscar,maximo)
-        mostrar_varios(aux_maximo,param_buscar,"La altura maxima es de : {}")
-    elif opcion == "E":
-        param_buscar = "altura"
-        minimo = sacar_min(lista_personajes,param_buscar)
-        aux_minimo = buscar_float(lista_personajes,param_buscar,minimo)
-        mostrar_varios(aux_minimo,param_buscar,"La altura minima es de :")
+    match opcion:       
+        case "A":
+            print("Listado de superheroes")
+            print("Nombre\t Identidad\t Empresa\t Altura\t Peso\t")
+            for superheroe in lista_heroes:
+                print(f"{superheroe['nombre']}\t {superheroe['identidad']}\t {superheroe['empresa']}\t {superheroe['altura']}\t {superheroe['peso']}\t")
+        case "B":
+            param_mostrar_1 = "nombre"
+            mostrar_lista_parcial(lista_heroes,"Lista de superheroes",param_mostrar_1)
+        case "C":
+            param_mostrar_1 = "nombre"
+            param_mostrar_2 = "altura"
+            mostrar_lista_parcial(lista_heroes,"Lista de superheroes",param_mostrar_1,param_mostrar_2)
+        case "D":
+            param_buscar = "altura"
+            maximo = sacar_max(lista_heroes,param_buscar)
+            aux_maximo = buscar_float(lista_heroes,param_buscar,maximo)
+            mostrar_varios(aux_maximo,param_buscar,"La altura maxima es de : {}")
+        case "E":
+            param_buscar = "altura"
+            minimo = sacar_min(lista_heroes,param_buscar)
+            aux_minimo = buscar_float(lista_heroes,param_buscar,minimo)
+            mostrar_varios(aux_minimo,param_buscar,"La altura minima es de :")
 
-    elif opcion == "F":
-        param_buscar = "altura"
-        promedio = sacar_promedio(lista_personajes,param_buscar)
-        mostrar(promedio,"El Promedio de altura de los superheroes es de: ")
-    elif opcion == "G":
-        maximo = sacar_max(lista_personajes,"altura")
-        minimo = sacar_min(lista_personajes,"altura")
+        case "F":
+            param_buscar = "altura"
+            promedio = sacar_promedio(lista_heroes,param_buscar)
+            mostrar(promedio,"El Promedio de altura de los superheroes es de: ")
+        case "G":
+            maximo = sacar_max(lista_heroes,"altura")
+            minimo = sacar_min(lista_heroes,"altura")
 
-        list_max = buscar_float(lista_personajes,"altura",maximo)
-        list_min = buscar_float(lista_personajes,"altura",minimo)
+            list_max = buscar_float(lista_heroes,"altura",maximo)
+            list_min = buscar_float(lista_heroes,"altura",minimo)
 
-        mostrar_varios(list_max,"nombre","La altura maxima pertenece al superheroe: ")
-        mostrar_varios(list_min,"nombre","La altura minima pertenece al superheroe: ")
- 
-    elif opcion == "H":
-        param_buscar = "peso"
-        maximo = sacar_max(lista_personajes,param_buscar)
-        minimo = sacar_min(lista_personajes,param_buscar)
-        list_max = buscar_float(lista_personajes,param_buscar,maximo)
-        list_min = buscar_float(lista_personajes,param_buscar,minimo)
+            mostrar_varios(list_max,"nombre","La altura maxima pertenece al superheroe: ")
+            mostrar_varios(list_min,"nombre","La altura minima pertenece al superheroe: ")
+    
+        case "H":
+            param_buscar = "peso"
+            maximo = sacar_max(lista_heroes,param_buscar)
+            minimo = sacar_min(lista_heroes,param_buscar)
+            list_max = buscar_float(lista_heroes,param_buscar,maximo)
+            list_min = buscar_float(lista_heroes,param_buscar,minimo)
 
-        mostrar_varios(list_max,"nombre","El peso mas alto pertenece al superheroe: ")
-        mostrar_varios(list_min,"nombre","El peso mas bajo pertenece al superheroe: ")
-
- 
-    elif opcion == "J":
-        elegir_submenu(lista_personajes,"Listado de Superheroes - {}")
-
-    elif opcion == "Z":
-        print("Saliendo del programa")    
-        break
-    else:
-        print("Opcion invalida.")
+            mostrar_varios(list_max,"nombre","El peso mas alto pertenece al superheroe: ")
+            mostrar_varios(list_min,"nombre","El peso mas bajo pertenece al superheroe: ")
+        case "I":
+            elegir_submenu(lista_heroes,"Listado de Superheroes - {}")
+        case "J":
+            elegir_menu_stark_01(lista_heroes)
+        case "Z":
+            print("Saliendo del programa")    
+            break
+        case _:
+            print("Opcion invalida.")
 
     os.system('pause')
 
